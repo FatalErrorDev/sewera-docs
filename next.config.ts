9fb7881644a13c174130b7e97036ec9e1d9db530
FatalErrorDev <tomitus8@gmail.com>
2026-03-13 15:24:58 +0100

    pages fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/sewera-docs",
+  assetPrefix: "/sewera-docs/",
   images: { unoptimized: true },
 };
 
